be11803d3b45da3829cf7bf72a0e1a6872388f99
JOSnow985 <jaden.olvera@students.snow.edu>
2025-11-19 11:07:59 -0700

    Initial file handling

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Jaden Olvera, CS-1400, Lab 11 madlibs 2
+class Program
+{
+    static void Main(string[] filenames)
+    {
+        Dictionary<string, List<string>> wordCategories = [];
+        List<List<string>> storyWords = [];
+        foreach(string filename in filenames)
+        {
+            // Load current filename into a string array
+            string[] storyLines = File.ReadAllLines(filename);
+
+            // Split into words based on whitespaces and Add to storyWords
+            foreach (string line in storyLines)
+            {
+                storyWords.Add(line.Split(" ").ToList());
+            }
+        }
+    }
+}
