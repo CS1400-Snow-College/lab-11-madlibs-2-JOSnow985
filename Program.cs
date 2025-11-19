aa079699f7d3bfdec4fd84fe8652eafd0c3e371b
JOSnow985 <jaden.olvera@students.snow.edu>
2025-11-19 12:20:08 -0700

    Word processing and Dictionary construction

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// Jaden Olvera, CS-1400, Lab 11 madlibs 2
+using System.Text;
+
 class Program
 {
     static void Main(string[] filenames)
@@ -14,6 +16,69 @@ class Program
             foreach (string line in storyLines)
             {
                 storyWords.Add(line.Split(" ").ToList());
+            }
+        }
+
+        // Processing each story
+        for (int storyIndex = 0; storyIndex < storyWords.Count; storyIndex++)
+        {
+            // Processing each list of words
+            for (int wordListIndex = 0; wordListIndex < storyWords[storyIndex].Count; wordListIndex++)
+            {
+                // If the word we're looking at contains a :, the word and key need to be in the dictionary
+                if (storyWords[storyIndex][wordListIndex].Contains(':'))
+                {
+                    bool foundKeyStart = false;
+                    StringBuilder buildWordToAdd = new();
+                    StringBuilder buildKeyToAdd = new();
+                    for (int letterIndex = 0; letterIndex < storyWords[storyIndex][wordListIndex].Length; letterIndex++)
+                    {
+                        if (foundKeyStart == false)
+                        {
+                            if (storyWords[storyIndex][wordListIndex][letterIndex] != ':')
+                            {
+                                buildWordToAdd.Append(storyWords[storyIndex][wordListIndex][letterIndex]);
+                            }
+                            else if (storyWords[storyIndex][wordListIndex][letterIndex] == ':')
+                            {
+                                foundKeyStart = true;
+                            }
+                        }
+
+                        else if (foundKeyStart == true)
+                        {
+                            if (storyWords[storyIndex][wordListIndex][letterIndex] != ':')
+                            {
+                                if (!".,! ".Contains(storyWords[storyIndex][wordListIndex][letterIndex]))
+                                {
+                                    buildKeyToAdd.Append(storyWords[storyIndex][wordListIndex][letterIndex]);
+                                }
+                                else if (".,! ".Contains(storyWords[storyIndex][wordListIndex][letterIndex]))
+                                {
+                                    // buildWordToAdd.Append(storyWords[storyIndex][wordListIndex][letterIndex]);
+                                    storyWords[storyIndex].Insert(wordListIndex + 1, $"{storyWords[storyIndex][wordListIndex][letterIndex]}");
+                                }
+                            }
+                        }
+                    }
+                    string wordToAdd = buildWordToAdd.ToString();
+                    string keyToAdd = buildKeyToAdd.ToString();
+                    // Check if the dictionary already has this key in it
+                    // If it does, check if that key already has that word in it
+                    if (wordCategories.ContainsKey(keyToAdd))
+                    {
+                        if (!wordCategories[keyToAdd].Contains(wordToAdd))
+                        {
+                            wordCategories[keyToAdd].Add(wordToAdd);
+                        }
+                    }
+                    else
+                    {
+                        List<string> ListToAdd = [wordToAdd];
+                        wordCategories.Add(keyToAdd, ListToAdd);
+                    }
+                    storyWords[storyIndex][wordListIndex] = keyToAdd;
+                }
             }
         }
     }
